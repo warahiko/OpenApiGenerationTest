76083f8c331b526f220957316d1a02a1f956fe22
Go Shibata <g_shibata@yumemi.co.jp>
2020-09-24 19:10:58 +0900

    rename app/build.gradle -> app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(30)
+    buildToolsVersion("30.0.1")
+
+    defaultConfig {
+        applicationId = "com.example.openapigenerationtest"
+        minSdkVersion(16)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), file("proguard-rules.pro"))
+        }
+    }
+}
+
+val kotlinVersion: String by rootProject.extra
+
+dependencies {
+    implementation(fileTree("dir" to "libs", "include" to listOf("*.jar")))
+
+    implementation(kotlin("stdlib", version = kotlinVersion))
+    implementation("androidx.core", "core-ktx", "1.3.1")
+    implementation("androidx.appcompat", "appcompat", "1.2.0")
+    implementation("androidx.constraintlayout", "constraintlayout", "2.0.1")
+    testImplementation("junit", "junit", "4.12")
+    androidTestImplementation("androidx.test.ext", "junit", "1.1.2")
+    androidTestImplementation("androidx.test.espresso", "espresso-core", "3.3.0")
+}

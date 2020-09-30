7fb06a5217b9c61697fdc301a6c174585e4b97e8
Go Shibata <g_shibata@yumemi.co.jp>
2020-09-30 18:52:04 +0900

    MainViewModel で呼び出すように変更 レスポンスを表示するように変更 ネットワークを使えるようにマニフェスト変更

--- app/src/main/java/com/example/openapigenerationtest/MainActivity.kt
+++ app/src/main/java/com/example/openapigenerationtest/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.openapigenerationtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.itemButton
+import kotlinx.android.synthetic.main.activity_main.itemText
+import kotlinx.android.synthetic.main.activity_main.userButton
+import kotlinx.android.synthetic.main.activity_main.userText
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +16,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.users.observe(this, Observer { users ->
+            userText.text = users.joinToString(separator = ",\n", prefix = "[\n", postfix = "\n]")
+        })
+        viewModel.items.observe(this, Observer { items ->
+            itemText.text = items.joinToString(separator = ",\n", prefix = "[\n", postfix = "\n]")
+        })
+
+        userButton.setOnClickListener {
+            viewModel.getAllUsers()
+        }
+        itemButton.setOnClickListener {
+            viewModel.getAllItems()
+        }
     }
 }
--- app/src/main/java/com/example/openapigenerationtest/MainViewModel.kt
+++ app/src/main/java/com/example/openapigenerationtest/MainViewModel.kt
@@ -1,5 +1,49 @@
 package com.example.openapigenerationtest
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.openapigenerationtestapi.sp.model.Item
+import com.example.openapigenerationtestapi.sp.model.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MainViewModel : ViewModel()
+class MainViewModel : ViewModel() {
+
+    private val _users = MutableLiveData<Array<User>>()
+    val users: LiveData<Array<User>>
+        get() = _users
+
+    private val _items = MutableLiveData<Array<Item>>()
+    val items: LiveData<Array<Item>>
+        get() = _items
+
+    fun getAllUsers() {
+        QiitaUserService().getAllUser(1, 20, object : Callback<Array<User>> {
+            override fun onFailure(call: Call<Array<User>>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+            override fun onResponse(call: Call<Array<User>>, response: Response<Array<User>>) {
+                if (!response.isSuccessful) return
+
+                _users.postValue(response.body())
+            }
+        })
+    }
+
+    fun getAllItems() {
+        QiitaItemService().getAllItems(1, 20, "", object : Callback<Array<Item>> {
+            override fun onFailure(call: Call<Array<Item>>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+            override fun onResponse(call: Call<Array<Item>>, response: Response<Array<Item>>) {
+                if (!response.isSuccessful) return
+
+                _items.postValue(response.body())
+            }
+        })
+    }
+}
